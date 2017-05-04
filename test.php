95abd33db9dcce9b545d632656a711d505d5ca31
Anass Rahou <nsrahou@gmail.com>
2017-05-04 02:20:40 -0500

    Test file included

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Require main class file
+ */
+require_once dirname(__FILE__) . '/MySQLi_Handler.php';
+
+/**
+ * Setup database connection with Singleton pattern.
+ * @var object
+ */
+$db = MySQLi_Handler::getInstance([
+    'hostname' => 'localhost',
+    'username' => 'homestead',
+    'password' => 'secret',
+    'database' => 'anass',
+]);
+
+/**
+ * Make a simple query to the database
+ * @var object
+ */
+$result = $db->query("SELECT * FROM names");
+
+var_dump($result->results('assoc'));
