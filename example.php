b7565f0774ad5a2a1973f8a3a44b037e39160139
Anass Rahou <nsrahou@gmail.com>
2017-05-06 19:10:30 -0500

    Examples of how to use the class

---
+++ example.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Require main class file
+ */
+require_once dirname(__FILE__) . '/database.php';
+
+/**
+ * Setup database connection with Singleton pattern.
+ * @var object
+ */
+$db = MySQLi_Database::getInstance([
+    'hostname' => 'localhost',
+    'username' => 'homestead',
+    'password' => 'secret',
+    'database' => 'examples',
+]);
+
+$content = [
+    'table' => 'names',
+    'fname' => 'Anass',
+    'lname' => 'Rahou',
+    'age'   => 28,
+    'email' => 'anass@example.com',
+];
+
+$conditions = [
+    'id'    => 28,
+    'email' => 'anass@example.com',
+];
+
+/**
+ * Check if have results.
+ */
+if ($db->exists($content)) {
+
+    /**
+     * Foreach result content.
+     */
+    foreach ($db->results('assoc') as $key => $value) {
+        echo $value, ", ";
+    }
+
+} else {
+
+    /**
+     * Insert content.
+     */
+    $db->insert($content);
+
+}
+
+/**
+ * Update database content.
+ */
+if ($db->update($content, $conditions)) {
+
+    echo 'User has been updated.';
+
+}
+
+/**
+ * Find fname value and delete row.
+ */
+$delete = [
+    'table' => 'names',
+    'fname' => 'Anass',
+];
+
+/**
+ * Delete from 'names' where 'fname' = 'Anass'.
+ */
+if ($db->delete($delete)) {
+
+    echo 'User has been deleted.';
+
+}
