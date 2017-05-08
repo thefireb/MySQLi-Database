5d06d848ee9b7a58a93b5f36cd4abc2e18eb4fbf
Anass Rahou <nsrahou@gmail.com>
2017-05-08 15:56:22 -0500

    New minor update

--- example.php
+++ example.php
@@ -9,69 +9,83 @@ require_once dirname(__FILE__) . '/database.php';
  * Setup database connection with Singleton pattern.
  * @var object
  */
-$db = MySQLi_Database::getInstance([
+$db = Database::getInstance([
     'hostname' => 'localhost',
     'username' => 'homestead',
     'password' => 'secret',
     'database' => 'examples',
 ]);
 
-$content = [
-    'table' => 'names',
-    'fname' => 'Anass',
-    'lname' => 'Rahou',
-    'age'   => 28,
-    'email' => 'anass@example.com',
-];
-
-$conditions = [
-    'id'    => 28,
-    'email' => 'anass@example.com',
-];
-
 /**
- * Check if have results.
+ * Check if connected to the database.
  */
-if ($db->exists($content)) {
+if ($db->isConnected()) {
+    
+    $content = [
+        'table' => 'names',
+        'fname' => 'Anass',
+        'lname' => 'Rahou',
+        'age'   => 28,
+        'email' => 'anass@example.com',
+    ];
+
+    $conditions = [
+        'id'    => 28,
+        'email' => 'anass@example.com',
+    ];
 
     /**
-     * Foreach result content.
+     * Check if have results.
      */
-    foreach ($db->results('assoc') as $key => $value) {
-        echo $value, ", ";
+    if ($db->exists($content)) {
+
+        /**
+         * Foreach result content.
+         */
+        foreach ($db->results('assoc') as $key => $value) {
+            echo $value, ", ";
+        }
+
+    } else {
+
+        /**
+         * Insert content.
+         */
+        $db->insert($content);
+
+    }
+
+    /**
+     * Update database content.
+     */
+    if ($db->update($content, $conditions)) {
+
+        echo 'User has been updated.';
+
+    }
+
+    /**
+     * Find fname value and delete row.
+     */
+    $delete = [
+        'table' => 'names',
+        'fname' => 'Anass',
+    ];
+
+    /**
+     * Delete from 'names' where 'fname' = 'Anass'.
+     */
+    if ($db->delete($delete)) {
+
+        echo 'User has been deleted.';
+
     }
 
 } else {
 
     /**
-     * Insert content.
+     * Fire Database error message.
      */
-    $db->insert($content);
-
-}
-
-/**
- * Update database content.
- */
-if ($db->update($content, $conditions)) {
-
-    echo 'User has been updated.';
-
-}
-
-/**
- * Find fname value and delete row.
- */
-$delete = [
-    'table' => 'names',
-    'fname' => 'Anass',
-];
-
-/**
- * Delete from 'names' where 'fname' = 'Anass'.
- */
-if ($db->delete($delete)) {
-
-    echo 'User has been deleted.';
+    echo $db->errorMessage();
 
 }
