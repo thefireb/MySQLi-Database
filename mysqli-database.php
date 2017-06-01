bb62a994fd527e9810e08aabf5b9d0907dd9c629
Anass Rahou <nsrahou@gmail.com>
2017-05-31 23:36:57 -0500

    Update mysqli-database.php

--- mysqli-database.php
+++ mysqli-database.php
@@ -8,19 +8,6 @@
  * This typically occurs when we have global objects (such as a Configuration class)
  * or a shared resource (such as an event queue).
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  * @author     Anass Rahou
  * @package    MySQLi_Handler
  * @copyright  Copyright (c) 2016 - 2017
